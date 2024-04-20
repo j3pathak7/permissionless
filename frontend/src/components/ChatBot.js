5103545493c52b4f73e31098da84b6593b1bbf9d
Jyotirmoy Pathak <jyotirmoypathak.37@gmail.com>
2024-04-20 21:24:19 +0530

    c

--- frontend/src/components/ChatBot.js
+++ frontend/src/components/ChatBot.js
@@ -16,7 +16,7 @@ const ChatBot = () => {
 
     try {
       const res = await axios.post(
-        "http://127.0.0.1:8000/chat",
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/chat`,
         { message },
         {
           headers: {
